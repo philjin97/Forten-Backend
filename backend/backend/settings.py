ea02f0892cdf23e8de6d2a69d1a35b78c79e42c0
Phil Jin Sang <philjin97@gmail.com>
2024-01-10 00:25:32 +0900

    chore: django, mysql 연동

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'backend',
+        'USER': 'user',
+        'PASSWORD': 'pass',
+        'HOST': 'db', 
+        'PORT': '3306'
     }
 }
+
 
 
 # Password validation

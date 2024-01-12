d322d29351a2bec604a36bdfa09cb1e704b3e9b9
여민지 <yeomj051@naver.com>
2024-01-12 21:39:43 +0900

    chore: docker nginx_gunicorn 세팅

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,12 +14,36 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf import settings
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="FORTEN API",
+        default_version='v1',
+        description="회원, 학생, 성적, 평가, 시각화, 프롬트에 관한 API",
+        terms_of_service="https://github.com/Techeer-H/backend",
+        contact=openapi.Contact(email="yeomj051@naver.com"),
+        license=openapi.License(name="Forten License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
-    path('student/', include('student.urls')),
-    path('feedback/', include('feedback.urls')),
-    path('analysis/', include('analysis.urls')),
+    path('api/user/', include('user.urls')),
+    path('api/student/', include('student.urls')),
+    path('api/feedback/', include('feedback.urls')),
+    path('api/analysis/', include('analysis.urls')),
 ]
+
+# swagger
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
+        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),    ]
--- backend/feedback/models.py
+++ backend/feedback/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from student.models import Student
+from user.models import User
 
 # Create your models here.
+class Feedback(models.Model):
+	id = models.AutoField(primary_key=True)
+	user_id = models.ForeignKey(User, related_name="user", on_delete=models.CASCADE, null=False)
+	student_id = models.ForeignKey(Student, related_name="student_feedback", on_delete=models.CASCADE, null=False)
+	student_rating = models.BigIntegerField(null=False)
+	parent_rating = models.BigIntegerField(null=True)
+	content = models.TextField(max_length=500, null=False)
+	created_at = models.DateTimeField(auto_now_add=True, null=False)
+	updated_at = models.DateTimeField(auto_now=True, null=True)

ba95d68573883a088857cd733edcdaba3c88bf5d
여민지 <yeomj051@naver.com>
2024-01-16 17:30:24 +0900

    feat: 즐겨찾기 관련 API추가 및 swagger 수정

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,4 +1,4 @@
-from .models import User
+from .models import User, Favorite
 from rest_framework import serializers
 
 class UserRegisterSerializer(serializers.ModelSerializer):
@@ -15,3 +15,13 @@ class UserMemoSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'memo']
+
+class UserFavoriteGetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Favorite
+        fields = ['student_id']
+
+class UserFavoriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Favorite
+        fields = '__all__'

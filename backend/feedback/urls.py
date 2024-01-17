1d5e012bfd8fa9be3e317cab436a5dc9c21f3580
여민지 <yeomj051@naver.com>
2024-01-17 14:26:07 +0900

    fix:feedbakc url.py 수정

--- backend/feedback/urls.py
+++ backend/feedback/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:user_id>/<int:student_id>/info', views.FeedbackGetAPIView.as_view()),
+    path('<int:user_id>/<int:student_id>/info/', views.FeedbackGetAPIView.as_view()),
     path('<int:user_id>/', views.FeedbackRegisterAPIView.as_view()),
     path('<int:user_id>/<int:feedback_id>/', views.FeedbackPutDeleteAPIView.as_view()),
 

7198a051513a39bab950dfbcc09339c40db80cfc
Phil Jin Sang <philjin97@gmail.com>
2024-01-19 01:23:57 +0900

    fix: Temporary Prompt

--- backend/analysis/models.py
+++ backend/analysis/models.py
@@ -3,7 +3,7 @@ from student.models import Student
 
 # Create your models here.
 
-class TemporaryPrompt(models.Model):
-    id = models.AutoField(primary_key=True)
-    student_id = models.ForeignKey(Student, related_name="student_prompt", on_delete=models.CASCADE, null=False, db_column="student_id")
-    prompt = models.TextField()
+# class TemporaryPrompt(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     student_id = models.ForeignKey(Student, related_name="student_prompt", on_delete=models.CASCADE, null=False, db_column="student_id")
+#     prompt = models.TextField()
